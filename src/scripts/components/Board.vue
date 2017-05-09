61dccfdbeb7e7fee6bc81e5ee3b0370bdcc3c874
Liruizhen <617811525@qq.com>
2017-05-09 11:20:34 +0800

    5.9mock数据

--- src/scripts/components/Board.vue
+++ src/scripts/components/Board.vue
@@ -1,12 +1,12 @@
 <template>
-	<div class="m-board">
+<div class="m-board">
 	
 	<header class="yo-header yo-header-a">
       <h2 class="title">必要</h2>
       <span class="regret yo-ico">&#xf067;</span>
 	</header>
 	
-
+	<div class="board-section">
 	<mt-swipe :auto="4000">
 	    <mt-swipe-item v-for="item in bannerimglist" :key="item">
 	      <img class="lb" :src="item.webpImageUrl"/>
@@ -20,11 +20,11 @@
 		<span class="yo-ico saynew-right">&#xf07f;</span>
 	</div>
 	
-	<div class="new">
-		<img class="left" src="http://bfs.biyao.com/group1/M00/00/75/wKhkVVkC6_2AXP6gAAEJNJjr17Q252.jpg" />
+	<div class="new" >
+		<img class="left"  v-lazy="newimgs[0].webpImageUrl" />
 		<div class="right">
-			<img class="saynew-right-top" src="http://bfs.biyao.com/group1/M00/01/27/wKhkVFkMKO6AOcQcAAB78YLr0k0785.jpg"/>
-			<img src="http://bfs.biyao.com/group1/M00/00/75/wKhkVVkC69GAGc6fAACS2D7m7vI709.jpg"/>
+			<img class="saynew-right-top" v-lazy="newimgs[1].webpImageUrl"/>
+			<img v-lazy="newimgs[2].webpImageUrl"/>
 		</div>
 	</div>
 	
@@ -37,48 +37,38 @@
 		</div>
 	</div>
 	
-	<ul class="main">
 	
-	<li>
-		<h2 >光学眼镜<span class="yo-ico">&#xf07f;</span></h2>
+	<mt-loadmore :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" ref="loadmore">
+
+<ul class="main">
+	<li v-for="item in maindata">
+		<h2 >{{item.moduleTitle}}<span class="yo-ico">&#xf07f;</span></h2>
 		<div class="imgbig">
-			<img src="https://bfs.biyao.com/group1/M00/00/0E/wKhkVFjq8P6AVIOyAACWXaInagc630.jpg" />
+			<img :src="item.moduleImage" />
 			<div class="box">
 				<div class="text">
-					<span>依视路集团旗下</span>
-					<span>企业制造</span>
+					<span>{{item.manufacturers}}</span>
+					<span>{{item.moduleBrand}}</span>
 				</div>
 			</div>
 		</div>
 		
 		<div class="text">
 		<ul>
-		<li>
-			<img src="https://img.biyao.com/files/temp/d3/d3c25e12d968b3a7.jpg"/>
-			<p>负担-大股东</p>
-			<p><i>￥</i><b>223</b></p>
+		<li v-for="item in item.moduleItems">
+			<img :src="item.image"/>
+			<p>{{item.ext.itemName}}</p>
+			<p><i>￥</i><b>{{item.ext.itemPrice}}</b></p>
 		</li>
-		<li>
-			<img src="https://img.biyao.com/files/temp/d3/d3c25e12d968b3a7.jpg"/>
-			<p>负担-大股东</p>
-			<p><i>￥</i><b>223</b></p>
-		</li>
-		<li>
-			<img src="https://img.biyao.com/files/temp/d3/d3c25e12d968b3a7.jpg"/>
-			<p>负担-大股东</p>
-			<p><i>￥</i><b>223</b></p>
-		</li>
-		<li>
-			<img src="https://img.biyao.com/files/temp/d3/d3c25e12d968b3a7.jpg"/>
-			<p>负担-大股东</p>
-			<p><i>￥</i><b>223</b></p>
-		</li>
+		
 		</ul>
 		</div>
 	</li>
 	
 	</ul>
-	
+		 
+</mt-loadmore>
+	</div>
 
 </div>
 </template>
@@ -87,19 +77,34 @@
 <script>
 import Vue from "vue"
 import utilAxios from '../utils/axios'
-import { Swipe, SwipeItem } from 'mint-ui'
+import { Swipe, SwipeItem ,Lazyload,Loadmore} from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
+Vue.use(Lazyload);
 Vue.component(Swipe.name, Swipe);
-Vue.component(SwipeItem.name, SwipeItem);
-  
+Vue.component(Loadmore.name, Loadmore);
+Vue.component(SwipeItem.name, SwipeItem); 
+ 
 export default{
 	data(){
 	    return {
 	      bannerimglist:[],
-	      saynew:""
+	      saynew:"",
+	      newimgs:[],
+	      maindata:[],
+	      allLoaded:false
 	    }
   },
+  methods:{
+	
+	loadBottom: function(){
+		console.log(1)
+		this.allLoaded = true
+  		this.$refs.loadmore.onBottomLoaded()
+	}
+	
+  },
+  
 	mounted: function () {
     var that = this
     utilAxios.get({
@@ -108,6 +113,13 @@ export default{
       callback: function (res) {
       	that.bannerimglist=res.data.banners
       	that.saynew=res.data.article.ext.title
+        that.newimgs= res.data.modules[0].moduleInfo.moduleItems
+       
+        for(let i=0; i<res.data.modules.length; i++){
+        	if(res.data.modules[i].moduleType==3){
+        		that.maindata.push(res.data.modules[i].moduleInfo)
+        	}
+        }
       }
     })
   }
