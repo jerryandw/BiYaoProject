233aca876f00f084bd70f685418ec825985cc610
nuan1993 <xiaonieming@163.com>
2017-05-10 19:54:10 +0800

    liebiaotimu

--- static/mock/mock.js
+++ static/mock/mock.js
@@ -1,7 +1,13 @@
 let home = require('./home.json')
 
+let classify = require('./classify.json')
+
+let product = require('./product.json')
+
 module.exports = function () {
   return {
-    'home': home
+    'home': home, 
+    'classify':classify,
+    'product':product
   }
 }
