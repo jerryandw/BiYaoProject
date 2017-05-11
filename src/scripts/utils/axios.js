4df53f9fad804b9b91328ad664f27fad3189aef7
jerryangw <13031144478@163.com>
2017-05-11 17:54:32 +0800

    更新5月10号

--- src/scripts/utils/axios.js
+++ src/scripts/utils/axios.js
@@ -9,5 +9,20 @@ export default {
     .then(function (res) {
       param.callback(res)
     })
+  },
+  post: function(opt){
+    axios.post(opt.url, {
+      params: opt.data,
+      headers:{
+        'Content-Type':'application/x-www-form-urlencoded'
+      }
+    })
+    .then(function(res){
+      console.log(res)
+      opt.callback(res)
+    })
+    .catch(function(error){
+      console.log(error);
+    });
   }
 }
